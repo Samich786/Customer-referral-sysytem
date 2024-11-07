e1970a395efc977f9b4cf5bf5458b47376ab8fe4
Samich786 <msamisignup@gmail.com>
2024-11-07 19:49:14 +0500

    cms setup

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script setup>
+const route = useRoute();
+const router = useRouter();
+import { onMounted } from "vue";
+onMounted(() => {
+  router.push("/dashboard");
+});
+</script>
+
+<style></style>
